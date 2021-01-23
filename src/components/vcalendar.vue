66869bf223ca8931a9aa90b3df02876676f2aff9
Benicio Cardozo <beniciocardozomdp@gmail.com>
2021-01-23 15:43:39 -0300

    Add Footer

--- src/components/vcalendar.vue
+++ src/components/vcalendar.vue
@@ -2,18 +2,34 @@
   <v-date-picker
     :attributes="booked_dates"
     :min-date="new Date()"
-    :value="current_date"
+    :value="current_date || undefined"
     mode="dateTime"
     is24hr
     :disabled-dates="disabled_dates"
     @input="update_date_value($event)"
+    ref="calendar"
   >
     <template v-slot="{ inputValue, inputEvents }">
       <b-form-input
         placeholder="Fecha del Taller"
         :value="inputValue"
         v-on="inputEvents"
+        readonly
       />
+    </template>
+    <template #footer>
+      <span class="footer_of_calendar">
+        <b-button @click="save_value()" size="sm" variant="outline-info">
+          Guardar
+        </b-button>
+        <b-button
+          @click="handle_cancelation()"
+          size="sm"
+          variant="outline-danger"
+        >
+          Cancel
+        </b-button>
+      </span>
     </template>
   </v-date-picker>
 </template>
@@ -22,12 +38,18 @@
   export default {
     props: ["current_date"],
     methods: {
+      save_value() {
+        this.$refs.calendar.updateValue(this.current_date, {
+          formatDate: true,
+          hidePopover: true,
+        });
+      },
+      handle_cancelation() {
+        this.$refs.calendar.updateValue("", { hidePopover: true });
+      },
       update_date_value(event) {
         this.$emit("update:current_date", event);
       },
-    },
-    mounted() {
-      console.log(this.current_date);
     },
     computed: {
       disabled_dates() {
@@ -59,4 +81,13 @@
   // Date
 </script>
 
-<style scoped></style>
+<style scoped>
+  input {
+    background-color: rgb(255, 255, 255) !important;
+  }
+  .footer_of_calendar {
+    display: flex;
+    justify-content: space-around;
+    margin: 1vh 0;
+  }
+</style>
